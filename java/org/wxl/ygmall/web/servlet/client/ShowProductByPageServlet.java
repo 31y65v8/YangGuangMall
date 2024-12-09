3c07b0411433bbc4b164cb1d449f031845224db7
31y65v8 <167323762+31y65v8@users.noreply.github.com>
2024-12-09 22:10:11 +0800

    Update

--- java/org/wxl/ygmall/web/servlet/client/ShowProductByPageServlet.java
+++ java/org/wxl/ygmall/web/servlet/client/ShowProductByPageServlet.java
@@ -16,25 +16,25 @@ public class ShowProductByPageServlet extends HttpServlet {
 	}
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// 1.定义当前页码，默认为1
+		// 定义当前页码，默认为1
 		int currentPage = 1;
 		String _currentPage = request.getParameter("currentPage");
 		if (_currentPage != null) {
 			currentPage = Integer.parseInt(_currentPage);
 		}
-		// 2.定义每页显示条数,默认为4
+		// 定义每页显示条数
 		int currentCount = 4;
 		String _currentCount = request.getParameter("currentCount");
 		if (_currentCount != null) {
 			currentCount = Integer.parseInt(_currentCount);
 		}
-		// 3.获取查找的分类
+		// 获取查找的分类
 		String category = "全部商品";
 		String _category = request.getParameter("category");
 		if (_category != null) {
 			category = _category;
 		}
-		// 4.调用service，完成获取当前页分页Bean数据.
+		// 调用service，完成获取当前页分页Bean数据.
 		ProductService service = new ProductService();
 		PageBean bean = service.findProductByPage(currentPage, currentCount,
 				category);
